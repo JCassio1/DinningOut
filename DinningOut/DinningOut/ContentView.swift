bf6dda4138f7d5393400f6fc942a6d967f0ed8b8
Mr. Robot <joselsondias@hotmail.com>
2021-03-06 22:24:48 +0000

    Xcode setup

---
+++ DinningOut/DinningOut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DinningOut
+//
+//  Created by MR.Robot 💀 on 06/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DinningOut/DinningOut/DinningOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinningOutApp.swift
+//  DinningOut
+//
+//  Created by MR.Robot 💀 on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DinningOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
